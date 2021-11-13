874e82845031ad7b554f0838cf3eebecbd97be65
BALA SAI CHUKKAPALLI <90022963+BalasaiChukkapalli@users.noreply.github.com>
2021-11-13 16:36:59 +0530

    Add files via upload

---
+++ palindromecheckeroutput.py
@@ -0,0 +1,7 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\chukk\Documents\desktop\PYTHONCISCO\palindromechecker.py ==
+Enter a word to check the palindrome: bala
+The given word bala is not Palindrome
+>>> 
